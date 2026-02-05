ce9af4881e9d632d4b1d2d764511f0eb329e3c8e
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-02-05 23:54:41 +0500

    Adjust run script to mount current folder

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-podman-compose --in-pod False run --rm env
+
+ENV_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(pwd)"
+
+echo "🔧 Configs loaded from: $ENV_ROOT"
+echo "📂 Mounting project:   $PROJECT_ROOT"
+
+PROJECT_PATH="$PROJECT_ROOT" podman-compose \
+  -f "$ENV_ROOT/compose.yml" \
+  --in-pod False \
+   run --rm env
