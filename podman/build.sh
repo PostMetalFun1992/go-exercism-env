6a231d8d1a827984d5640e5ff9e2e84881c42cc6
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2026-02-05 19:43:01 +0500

    First

---
+++ podman/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME="${1:-go-exercism-env}"
+
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+podman build \
+  -f ./Containerfile \
+  --build-arg USER_ID="$USER_ID" \
+  --build-arg GROUP_ID="$GROUP_ID" \
+  -t "$IMAGE_NAME" .
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+podman-compose --in-pod False run --rm env
